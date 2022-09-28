7d3448780a1f6bed31f07ace0d027c6c77dfd1de
Aarif-shazZ <shazaarif0@gmail.com>
2022-09-28 23:36:43 +0530

    Navbar commit

---
+++ my-react-app/src/Components/HomeNavbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TopNavbar from './TopNavbar'
+
+export default function Navbar() {
+  return (
+    <div>
+        <TopNavbar/>
+    </div>
+  )
+}
---
+++ my-react-app/src/Components/HomeNavbar/TopNavbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { AiOutlineMobile,AiOutlineUser } from "react-icons/ai";
+import { TbPhoneCall } from "react-icons/tb";
+import "./styles/TopNavbar.module.css"
+import { Box } from '@chakra-ui/react'
+export default function TopNavbar() {
+  return (
+    <div>
+    <Box w="100%" bg="#f2f3f8" h="40px" className='TopNavbar'>
+        <Box w="88%" m="auto" h="100%" display="flex" justifyContent="flex-end" alignItems="center" className='container'>
+            <Box  w="30%" display="flex" justifyContent="space-between" alignItems="center" className='container-items'>
+                <Box w="30%" display="flex" justifyContent="space-around" alignItems="center"  className='download-box'>
+                    <Box className='download-app'>
+                        <AiOutlineMobile/>
+                    </Box>
+                    <Box className='download-text'>
+                        <h3>Download App</h3>
+                    </Box>
+                </Box>
+                <Box w="30%" mr="-10%" display="flex" justifyContent="space-around" alignItems="center" className='Employers-item'>
+                        <Box>
+                            <AiOutlineUser/>
+                        </Box>
+                        <Box>
+                            <h3>For Employers</h3>
+                        </Box>
+                </Box>
+                <Box w="30%"  display="flex" justifyContent="space-around" alignItems="center" className='help-items'>
+                    <Box >
+                        <TbPhoneCall/>
+                    </Box>
+                    <Box ml="-50%">
+                        <h3>Help</h3>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    </Box>
+    </div> 
+  )
+}
