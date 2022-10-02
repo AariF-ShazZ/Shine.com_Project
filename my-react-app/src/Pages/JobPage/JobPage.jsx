f7dc319db331a10f3cd3c353e2a4f9c1ac996d62
Aarif-shazZ <shazaarif0@gmail.com>
2022-10-02 10:25:49 +0530

    done

--- my-react-app/src/Pages/JobPage/JobPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import JobLeftComponents from "../../Components/JobComponents/JobLeftComponents"
-export default function JobPage() {
-  return (
-    <div>
-        <JobLeftComponents/>
-    </div>
-  )
-}
